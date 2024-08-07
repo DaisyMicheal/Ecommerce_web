e48c1ff14645ea32076310b1e83652db6a6e2ec1
DaisyMicheal <phileoreal@gmail.com>
2024-08-07 21:21:53 +0100

    add landing page file to develop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   return (
-    <CartProvider>
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-
-        <Route path='/landing' element={<LandingPage />} />
-        <Route path='/gadget-store' element={<GadgetStorePage />} />
-        <Route path='/appliances' element={<AppliancesPage />} />
-
-        <Route path='/for-him' element={<ForHimPage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/for-her' element={<ForHerPage />} />
-        <Route path='/beauty' element={<BeautyPage />} />
-        <Route path='/checkout' element={<CheckoutPage />} />
-        <Route path='/shopNow' element={<ShopNow />} />
-      </Routes>
-    </CartProvider>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+    </Routes>
   )
 }
 
