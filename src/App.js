37bccd4d81e38b06418767b2f841d0db59ff0830
DaisyMicheal <phileoreal@gmail.com>
2024-08-13 13:50:11 +0100

    Implement payment  functionality

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import LandingPage from './Pages /LandingPage/LandingPage'
-import SeeAllPage from './Pages /LandingPage/SeeAllPage'
-import OfficialStore from './Pages /LandingPage/OfficialStore'
-import CheckoutPage from './Pages /LandingPage/CheckoutPage'
+
+import PaymentPage from './pages/PaymentPage'
+
 function App() {
   return (
     <CartProvider>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/checkout' element={<CheckoutPage />} />
-        <Route path='/landing' element={<LandingPage />} />
-        <Route path='/seeAll' element={<SeeAllPage />} />
-        <Route path='/officialStore' element={<OfficialStore />} />
+        <Route path='/' element={<PaymentPage />} />
+
+        <Route path='/payment' element={<PaymentPage />} />
       </Routes>
       <ToastContainer />
     </CartProvider>
---
+++ src/pages/PaymentPage.js
@@ -0,0 +1,11 @@
+import Payment from '../components /payment'
+
+const PaymentPage = () => {
+  return (
+    <div>
+      <Payment />
+    </div>
+  )
+}
+
+export default PaymentPage
